b4718679d9750b03516318ff0d5520e4ce8ba533
Chad Coleman <cjc2238@tc.columbia.edu>
2017-02-28 16:34:43 -0500

    processed text files for modeling

--- Data Processing.py
+++ Data Processing.py
@@ -19,11 +19,13 @@ dataframes = {
     "biology": pd.read_csv("biology.csv"),
     "travel": pd.read_csv("travel.csv"),
     "diy": pd.read_csv("diy.csv"),
+    "test": pd.read_csv("test.csv"),
 }
 
 ## Remove html tags and uris from contents
 
 uri_re = r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))'
+
 def stripTagsAndUris(x):
     if x:
         # BeautifulSoup on content
@@ -37,9 +39,9 @@ def stripTagsAndUris(x):
         return re.sub(uri_re, "", text)
     else:
         return ""
-        for df in dataframes.values(): df["content"] = df["content"].map(stripTagsAndUris)
-print(dataframes["robotics"].iloc[1]) 
-
+for df in dataframes.values(): 
+    df["content"] = df["content"].map(stripTagsAndUris)
+    
 ##  Remove Punctuation
 
 def removePunctuation(x):
@@ -49,6 +51,35 @@ def removePunctuation(x):
     x = re.sub(r'[^\x00-\x7f]',r' ',x)
     # Removing (replacing with empty spaces actually) all the punctuations
     return re.sub("["+string.punctuation+"]", " ", x)
-    for df in dataframes.values(): df["title"] = df["title"].map(removePunctuation)
+for df in dataframes.values(): 
+    df["title"] = df["title"].map(removePunctuation)
     df["content"] = df["content"].map(removePunctuation)
+## Remove Stop Words
+stops = set(stopwords.words("english"))
+def removeStopwords(x):
+    # Removing all the stopwords
+    filtered_words = [word for word in x.split() if word not in stops]
+    return " ".join(filtered_words)
+for df in dataframes.values():
+    df["title"] = df["title"].map(removeStopwords)
+    df["content"] = df["content"].map(removeStopwords)
+
+## Split the tag strings into a list of tags
+for df in dataframes.values():
+    # From a string sequence of tags to a list of tags
+    df["tags"] = df["tags"].map(lambda x: x.split())
+
+## Confirm text processing has been compelted.
+    
 print(dataframes["robotics"].iloc[1])
+print(dataframes["cooking"].iloc[1])
+print(dataframes["crypto"].iloc[1])
+print(dataframes["biology"].iloc[1])
+print(dataframes["travel"].iloc[1])
+print(dataframes["diy"].iloc[1])
+print(dataframes["test"].iloc[1])
+
+## Save processed data frames to CSV
+for name, df in dataframes.items():
+    # Saving to file
+    df.to_csv(name + "_clean.csv", index=False)
